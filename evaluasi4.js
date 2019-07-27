5caee16d76ff7510780b4b698764f2ed15c39e50
AmarKhoirudin <42802825+AmarKhoirudin@users.noreply.github.com>
2019-07-27 15:31:33 +0700

    Update evaluasi4.js

--- evaluasi4.js
+++ evaluasi4.js
@@ -1,8 +1,6 @@
 var capitalize = function (string) {
     if (arguments.length === 1) {
         return string.toUpperCase();
-    } else {
-        return arguments.map(function (el) { return el.toUpperCase(); });
     }
 }
 
